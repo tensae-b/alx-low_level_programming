732f9aeecc7cd04480621be14238c1fcd09d1743
tensae7654 <tensaeb2016@gmail.com>
2023-03-16 12:58:41 -0700

    c hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *prints the phrase
+ *
+ *Description: print phrase
+ *Return: zero if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(Mystr);
+
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Descriptions: prints phrase
+ * using printf
+ *
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
