453b90f8f5fc678a7a0174c8c949a12c730d62f6
tensae7654 <tensaeb2016@gmail.com>
2023-03-17 09:55:58 -0700

    c if else

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i<=122; i++)
+	{
+		printf(i);
+	}
+	
+	return (0);
+
+
+}
