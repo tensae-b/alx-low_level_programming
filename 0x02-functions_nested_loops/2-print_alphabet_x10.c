22f93f896d2403a0b352f0664449f2afee8914fe
tensae7654 <tensaeb2016@gmail.com>
2023-03-21 11:49:06 -0700

    c nested loop

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - is a fuction to print alphabet
+ * description prints all alphabet in lowercase
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_alphabet(void)
+{
+        char c;
+	int i;
+        for(i=0; i<10; i++)
+	{
+        	for (c = 'a'; c <= 'z'; c++)
+        	{
+                	_putchar(c);
+        	}
+	}
+        _putchar('\n');
+}
