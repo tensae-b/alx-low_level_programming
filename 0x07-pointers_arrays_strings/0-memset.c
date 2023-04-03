2e90119525a4436c9d510bca1ffdb7877d7d5c4d
tensae7654 <tensaeb2016@gmail.com>
2023-04-03 11:44:21 -0700

    more pointers

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+char *_memset(char *s, char b, unsigned int n)
+{
+	/**
+	 * declare an unsigned int
+	 * because we are storing a
+	 * value that will always be
+	 * non-negative (zero or positive)
+	*/
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
+	return (s);
+}
