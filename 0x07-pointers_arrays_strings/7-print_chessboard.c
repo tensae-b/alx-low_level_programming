46ada3cfd5d3358c9df496e85a56b87d34896674
tensae7654 <tensaeb2016@gmail.com>
2023-04-03 12:11:22 -0700

    more pointers

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,29 +1,28 @@
-#include "holberton.h"
+#include <stdio.h>
 
 /**
- * print_chessboard - prints the chess board
- *
+ * print_diagsums - print the sum of the two
+ *                 diagonals of a square matrix
+ *                 of integers.
  * @a: input pointer
+ * @size: size of the matrix
  *
- * Return: return nothing
+ * Return: nothing
 */
 
-void print_chessboard(char (*a)[8])
+void print_diagsums(int *a, int size)
 {
-	int i, j;
+	int i, S1, S2;
 
-	i = 0;
-	j = 0;
-	while (i < 64)
+	S1 = 0;
+	S2 = 0;
+
+	for (i = 0; i < (size * size); i++)
 	{
-		if (i % 8 == 0 && i != 0)
-		{
-			j = i;
-			_putchar('\n');
-		}
-
-		_putchar(a[i / 8][i - j]);
-		i++;
+		if (i % (size + 1) == 0)
+			S1 += a[i];
+		if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
+			S2 += a[i];
 	}
-	_putchar('\n');
+	printf("%d, %d\n", S1, S2);
 }
