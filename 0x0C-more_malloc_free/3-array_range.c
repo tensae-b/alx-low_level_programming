c98640218b042148046e27cd44dadc7dd023b9e9
tensae7654 <tensaeb2016@gmail.com>
2023-04-12 12:51:54 -0700

    malloc

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,64 +1,30 @@
-#include "main.h"
+#include <stdlib.h>
 
 /**
- * abs - calculates the absolute value of passed integer
- * @a: the integer whose absolute value will be calculated
- * Return: if a < 0, the negation of a i.e. -1 * a
- *		   otherwise return a without modification
- */
-int abs(int a)
-{
-	if (a < 0)
-	{
-		a *= -1;
-		return (-1 * a);
-	}
-	return (a);
-}
-
-/**
- * array_range - creates an array of integers between min and max (including
- * both min and max)
- * @min: the smallest and first integer in the array
- * @max: the biggest and last integer in the array
+ * array_range - creates an array of integers
  *
- * Return: NULL if min > max
- *		   NULL if malloc fails to allocate memory
- *		   array of ints between min and max, including min and max
- */
+ * @min: min number of elements
+ * @max: max number of elements
+ *
+ * Return: array pointer address
+ *         NULL if it fails
+*/
 
 int *array_range(int min, int max)
 {
-	int *arr;
-	int i;
-	int j = abs(max) - abs(min);
-
-	j = abs(j);
+	int i, *array;
 
 	if (min > max)
 		return (NULL);
 
-
-/*
-	if ((min < 0) && (max < 0))
-		j = (-1 * max) - (-1 * min);
-	else if ((max < 0) && (min > 0))
-		j = (-1 * max) - min;
-	else if ((min < 0) && (max > 0))
-		j = max - (-1 * min);
-*/
-
-
-	arr = malloc(sizeof(int) * (max - min) + 1);
-
-	if (!arr)
+	array = malloc((max - min + 1) * sizeof(int));
+	if (array == NULL)
 		return (NULL);
 
-	for (i = 0; i <= j; i++)
+	for (i = 0; min <= max; i++)
 	{
-		arr[i] = min;
+		array[i] = min;
 		min++;
 	}
-
-	return (arr);
+	return (array);
 }
